2cc359183d63161636c08f2dc8404814155d5dd2
Kevin Balana <kevb4u@gmail.com>
2025-07-29 03:11:43 -0700

    add copy and paste

--- hypr/start.sh
+++ hypr/start.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
 
+# initializing wallpaper
+hyprpaper &
+
+# NetworkManagerApplet
+# nm-applet --indicator &
+
+# Taskbar
 waybar &
+
+# dunst
+
+# copy / paste
+# wl-clipboard-history -t &
+wl-paste --type text --watch cliphist store &
+wl-paste --type image --watch cliphist store &
+rm "$HOME/.cache/cliphist/db" & # Delete history at reboot
